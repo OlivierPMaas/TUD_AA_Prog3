adffd4b0e21ad3f477d6607d5d50dd8fac4bf580
opmaas <O.P.Maas@student.tudelft.nl>
2018-01-19 13:00:50 +0100

    Maxcut function works on basis of weight-matrix

--- sdp_maxcut.m
+++ sdp_maxcut.m
@@ -4,12 +4,19 @@ function [Y, solvertime] = sdp_maxcut(W)
     ops = sdpsettings('solver','sedumi');
     Y = sdpvar(n,n);
 
+    % Objective function
     obj = 0;
     for idx2 = 1:n
         for idx1 = 1:idx2-1
-            obj = obj + 1/2*W(idx1,idx2)*(1-Y(idx1,idx2));
+            % SDP presupposes minimization problems,
+            % whereas we are dealing with a maximization problem.
+            % Of course, maximizing S is equivalent to minimizing -S.
+            % Hence, we subtract instead of add.
+            obj = obj - 1/2*W(idx1,idx2)*(1-Y(idx1,idx2));
         end
     end
+    
+    % Constraints
     cons = [Y >= 0];
     for idx = 1:n
         cons = [cons, Y(idx,idx) == 1];
