29831a11a65b372a956e76448ab84747d7752a29
opmaas <O.P.Maas@student.tudelft.nl>
2018-01-19 13:14:06 +0100

    Restructured project

---
+++ java/test/GraphGeneratorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphGeneratorTest {
+
+    @Test
+    void Test1() {
+        GraphGenerator.main();
+    }
+}
